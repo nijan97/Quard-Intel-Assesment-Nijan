6270b4140980e219c152ae77c6f1325ea94941ec
nijan97 <148521093+nijan97@users.noreply.github.com>
2024-12-30 11:07:32 +0530

    Update next.config.mjs

--- product-page/next.config.mjs
+++ product-page/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Configure the project for static export
+  distDir: 'out',   // Set the output directory for static files
+};
 
 export default nextConfig;
